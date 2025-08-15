50cff77c7b2e521fe4a98ce792a9f750ff855aa0
nogue <nogueira.gomes.miguel@gmail.com>
2025-08-15 20:35:41 +0100

    Revert "renamed DTO to external to simplify"
    This reverts commit 7ebf0f0255268e194a4b376fe94cb3897bd734cd.

--- src/main/java/org/nogs/model/external/EventExternal.java
+++
@@ -1,8 +0,0 @@
-package org.nogs.model.external;
-
-public class EventExternal {
-    public String title;
-    public String startTime;
-    public String description;
-    public String venueId;
-}
--- src/main/java/org/nogs/translator/EventTranslator.java
+++ src/main/java/org/nogs/translator/EventTranslator.java
@@ -1,26 +1,26 @@
 package org.nogs.translator;
 
-import org.nogs.model.external.EventExternal;
+import org.nogs.model.dto.EventDTO;
 import org.nogs.model.internal.Event;
 import java.time.LocalDateTime;
 
 public class EventTranslator {
 
-    public static EventExternal toExternal(Event event) {
-        EventExternal eventExternal = new EventExternal();
-        eventExternal.title = event.title;
-        eventExternal.startTime = event.startTime.toString();
-        eventExternal.description = event.description;
-        eventExternal.venueId = event.venueId;
-        return eventExternal;
+    public static EventDTO toDTO(Event event) {
+        EventDTO dto = new EventDTO();
+        dto.title = event.title;
+        dto.startTime = event.startTime.toString();
+        dto.description = event.description;
+        dto.venueId = event.venueId;
+        return dto;
     }
 
-    public static Event fromExternal(EventExternal eventExternal) {
+    public static Event fromDTO(EventDTO dto) {
         Event event = new Event();
-        event.title = eventExternal.title;
-        event.startTime = LocalDateTime.parse(eventExternal.startTime);
-        event.description = eventExternal.description;
-        event.venueId = eventExternal.venueId;
+        event.title = dto.title;
+        event.startTime = LocalDateTime.parse(dto.startTime);
+        event.description = dto.description;
+        event.venueId = dto.venueId;
         return event;
     }
 }
